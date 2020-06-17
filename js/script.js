9d19aa9a3d021fd53df228dd2ee47233d21fc98a
Stefano <stefano.zaccaria29@gmail.com>
2020-06-17 23:53:32 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function () {
+    if (age===)
+    var name = document.getElementById('name').value;
+    var surname = document.getElementById('surname').value;
+    var km = document.getElementById('km').value;
+    var over = document.getElementById('over65').value;
+    var maggiorenne = document.getElementById('maggiorenne').value;
+    var minorenne = document.getElementById('minorenne').value;
+
+    // console.log(name, surname, km, over, maggiorenne, minorenne);  
+})
