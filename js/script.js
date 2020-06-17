ca137761d91bb8201a27858a2bab5d5a41c417e3
Stefano <stefano.zaccaria29@gmail.com>
2020-06-18 00:35:18 +0200

    update all

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
 var btn = document.getElementById('btn');
+var second-btn = document.getElementById('second-btn');
 
 btn.addEventListener('click', function () {
     
@@ -14,13 +15,22 @@ btn.addEventListener('click', function () {
     var price = (km * kmPrice).toFixed(2);
     var youngSales = (price * 20 / 100).toFixed(2);
     var oldSales = (price * 40 / 100).toFixed(2);
+    var maxTrain = 100000;
+    var minTrain = 90000;
+    var minCarriage = 1;
+    var maxCarriage = 10;
 
     if (age == "minorenne") {
         price = (price - youngSales).toFixed(2);
     } else if ( age == "over65") {
         price = (price - oldSales).toFixed(2);
     }
+
+    var train = Math.floor(Math.random()*(maxTrain - minTrain + 1)) + minTrain;
+    var carriage = Math.floor(Math.random()*(maxCarriage - minCarriage + 1)) + minCarriage; 
     console.log(price);
     
     // console.log(name, surname, km, over, maggiorenne, minorenne);  
-})
+});
+
+second-btn .addEventListener('click', function () {})
